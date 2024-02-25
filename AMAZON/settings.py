73428d5f8c7321421afc2dd558d6ee84327bfe93
HemaniKalaga <150174116+HemaniKalaga@users.noreply.github.com>
2024-02-25 20:28:33 +0530

    Update settings.py

--- AMAZON/settings.py
+++ AMAZON/settings.py
@@ -135,8 +135,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='kalagahemani1234@gmail.com'
-EMAIL_HOST_PASSWORD='qekw lbub chlf hdob'
+EMAIL_HOST_USER='------------'
+EMAIL_HOST_PASSWORD='------------'
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
 EMAIL_PORT=587
